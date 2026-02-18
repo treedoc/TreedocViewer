647e4cd68a0d82cfad6ec68d31e0c6f8bd69c725
Jianwu Chen <jianwu.chen@cloudkitchens.com>
2026-02-17 22:39:08 -0800

    fix ghpage

--- v2/src/main.ts
+++ v2/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 import ToastService from 'primevue/toastservice'
@@ -13,7 +13,7 @@ import App from './App.vue'
 import Home from './views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: Home },
   ],
